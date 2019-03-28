530d5d1406265e7d53a067c16e5fab28d9c1180d
resnakk <masanchez3@miuandes.cl>
2019-03-28 20:09:49 -0300

    toma

--- lab1/app/models/user.rb
+++ lab1/app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
 	belongs_to :Ticket_order
+	def persisted?
+		true
+	end
+	
+	def most_expensive_ticket_bought
+		
+	end
 end
+
+
--- lab1/db/seeds.rb
+++ lab1/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do |i|
+  User.create(name: "User ##{$i}", last_name: "last#{$i}", email: "email#{$i}@.com", phone:"#{$i}", password: "#{$i}")
+end
+
+3.times do |i|
+  Event_venue.create(name: "Venue ##{i}", adress: "Adress#{i}", capacity: 100*{i})
+end
+
+3.times do |i|
+  Event_information.create(name: "Event ##{i}", description: "Desc#{i}", date: Time.now, event_venue: 1)
+end
+
+
